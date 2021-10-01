094a9acf9dba4add9cd5d51b33ef884a5ca24ce6
henrytanujaya <70674841+henrytanujaya@users.noreply.github.com>
2021-10-02 01:18:24 +0700

    SurvivalShoot

---
+++ Assets/Scripts/Enemy/EnemyAttack.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAttack : MonoBehaviour
+{
+    public int attackDamage = 10;
+    bool playerInRange;
+    public float timeBetweenAttacks = 0.5f;
+    float timer;
+
+    PlayerHealth playerHealth;
+    Animator anim;
+    GameObject player;
+    EnemyHealth enemyHealth;
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerHealth = player.GetComponent<PlayerHealth>();
+        anim = GetComponent<Animator>();
+        enemyHealth = GetComponent<EnemyHealth>();
+    }
+
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= timeBetweenAttacks && playerInRange && enemyHealth.currentHealth > 0)
+        {
+            Attack();
+        }
+        if (playerHealth.currentHealth <= 0)
+        {
+            anim.SetTrigger("PlayerDead");
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player && other.isTrigger == false)
+        {
+            playerInRange = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            playerInRange = false;
+        }
+    }
+
+
+    void Attack()
+    {
+        timer = 0f;
+        if (playerHealth.currentHealth > 0)
+        {
+            playerHealth.TakeDamage(attackDamage);
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    EnemyHealth enemyHealth;
+    UnityEngine.AI.NavMeshAgent nav;
+    Transform player;
+    PlayerHealth playerHealth;
+
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        playerHealth = player.GetComponent<PlayerHealth>();
+        enemyHealth = GetComponent<EnemyHealth>();
+        nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
+        {
+            nav.SetDestination(player.position);
+        }
+        else
+        {
+            nav.enabled = false;
+        }
+    }
+}
